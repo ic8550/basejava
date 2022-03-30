7112460c51c5d3dc2a6f36658887a9b432420b80
Igor Chernyavsky <ic8550@gmail.com>
2022-03-30 19:49:02 +0300

    Add Resume.equals()
    Override Object.equals().
    Set equality of resume by equality of resume.uuid.

--- src/club/swdev/webapp/model/Resume.java
+++ src/club/swdev/webapp/model/Resume.java
@@ -20,4 +20,12 @@ public class Resume {
     public String toString() {
         return uuid;
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) return true;
+        if (obj == null || getClass() != obj.getClass()) return false;
+        Resume resume = (Resume) obj;
+        return uuid.equals(resume.uuid);
+    }
 }
