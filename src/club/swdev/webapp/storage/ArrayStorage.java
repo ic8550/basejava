aff6277e71eb17b628a216e9800d57e93619ceb8
Igor Chernyavsky <ic8550@gmail.com>
2022-04-02 19:09:35 +0300

    Rewrite ArrayStorage as AbstractArrayStorage child
    Implement methods save(), update(), delete(), and getIndex().

--- src/club/swdev/webapp/storage/ArrayStorage.java
+++ src/club/swdev/webapp/storage/ArrayStorage.java
@@ -2,34 +2,10 @@ package club.swdev.webapp.storage;
 
 import club.swdev.webapp.model.Resume;
 
-import java.util.Arrays;
-
 /**
  * Array based storage for Resumes
  */
-public class ArrayStorage {
-    /**
-     * final int MAX_SIZE - storage parameter, the maximum capacity of the storage
-     */
-    private final int MAX_SIZE = 3;
-
-    private final Resume[] storage = new Resume[MAX_SIZE];
-
-    /**
-     * Size of nonempty (nonnull) part of storage[] -- Number of contiguous
-     * nonnull Resumes in the storage[] array, starting from the beginning
-     * of storage[].
-     */
-    private int size = 0;
-
-    /**
-     * Replaces all nonnull Resumes in storage[] with the null value.
-     */
-    public void clear() {
-        Arrays.fill(storage, 0, size, null);
-        size = 0;
-    }
-
+public class ArrayStorage extends AbstractArrayStorage {
     /**
      * Adds a club.swdev.webapp.model.Resume with a given uuid to the storage[],
      * provided such club.swdev.webapp.model.Resume is not there already.
@@ -48,11 +24,11 @@ public class ArrayStorage {
             System.out.println("\nERROR: save(): resume uuid=\"\"\n");
             return;
         }
-        if (size >= MAX_SIZE) {
+        if (size >= STORAGE_CAPACITY) {
             System.out.println("\nERROR: save(): storage is full; cannot add new resume\n");
             return;
         }
-        if (findIndex(resume.getUuid()) >= 0) {
+        if (getIndex(resume.getUuid()) >= 0) {
             System.out.println("\nERROR: update(): resume with uuid="
                     + "\"" + uuid + "\""
                     + " is already in storage\n");
@@ -75,7 +51,7 @@ public class ArrayStorage {
             System.out.println("\nERROR: update(): resume uuid=null\n");
             return;
         }
-        int index = findIndex(resume.getUuid());
+        int index = getIndex(resume.getUuid());
         if (index >= 0) {
             storage[index] = resume;
         } else {
@@ -83,20 +59,6 @@ public class ArrayStorage {
                     + "\"" + uuid + "\""
                     + " not found in storage\n");
         }
-    }
-
-    /**
-     * @return club.swdev.webapp.model.Resume with a given uuid or null if there is no such club.swdev.webapp.model.Resume in storage[].
-     */
-    public Resume get(String uuid) {
-        int index = findIndex(uuid);
-        if (index < 0) {
-            System.out.println("\nERROR: get(): resume with uuid="
-                    + "\"" + uuid + "\""
-                    + " not found in storage\n");
-            return null;
-        }
-        return storage[index];
     }
 
     /**
@@ -111,7 +73,7 @@ public class ArrayStorage {
                     + " not found in storage\n");
             return;
         }
-        int index = findIndex(uuid);
+        int index = getIndex(uuid);
         if (index < 0) {
             System.out.println("\nERROR: delete(): resume with uuid="
                     + "\"" + uuid + "\""
@@ -123,22 +85,7 @@ public class ArrayStorage {
         }
     }
 
-    /**
-     * @return array, contains only Resumes in storage (without null)
-     */
-    public Resume[] getAll() {
-        return Arrays.copyOf(storage, size);
-    }
-
-    /**
-     * @return int number of contiguous nonnull Resumes of the storage[] array,
-     * starting from the beginning of storage[].
-     */
-    public int size() {
-        return size;
-    }
-
-    private int findIndex(String uuid) {
+    protected int getIndex(String uuid) {
         for (int i = 0; i < size; i++) {
             if (storage[i].getUuid().equals(uuid)) {
                 return i;
