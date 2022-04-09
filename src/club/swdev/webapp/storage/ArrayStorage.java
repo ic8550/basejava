062663c2c750040b28207307c7e1e33f8df1cabe
Igor Chernyavsky <ic8550@gmail.com>
2022-04-09 03:49:15 +0300

    Move "storage[size - 1] = null" to template method
    Move "storage[size - 1] = null" statement from the implementation
    of the deleteElement() method in the ArrayStorage over to the
    template method in the AbstractArrayStorage.

--- src/club/swdev/webapp/storage/ArrayStorage.java
+++ src/club/swdev/webapp/storage/ArrayStorage.java
@@ -24,6 +24,5 @@ public class ArrayStorage extends AbstractArrayStorage {
 
     protected void deleteElement(int index) {
         storage[index] = storage[size - 1];
-        storage[size - 1] = null;
     }
 }
