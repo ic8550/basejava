a97fc88727386d675f347a684eb8227e12f40957
Igor Chernyavsky <ic8550@gmail.com>
2022-04-10 03:44:13 +0300

    Add MainReflection class
    A demo class for playing with Reflection API.
    Using Reflection API we get access to the Resume's uuid field
    and the Resume's toString() method. We call toString() method
    on the Resume using invoke() method.

---
+++ src/club/swdev/webapp/MainReflection.java
@@ -0,0 +1,24 @@
+package club.swdev.webapp;
+
+import club.swdev.webapp.model.Resume;
+
+import java.lang.reflect.Field;
+import java.lang.reflect.Method;
+
+public class MainReflection {
+    public static void main(String[] args) throws SecurityException, ReflectiveOperationException {
+        Resume resume = new Resume();
+        Class<Resume> resumeClass = Resume.class;
+        Field uuidField = resumeClass.getDeclaredFields()[0];
+        uuidField.setAccessible(true);
+        System.out.println("Field name: " + uuidField.getName());
+        System.out.println("Field value: " + uuidField.get(resume));
+        uuidField.set(resume, "new_uuid");
+        System.out.println("New field value: " + resume);
+        // TODO : invoke resume.toString via reflection
+        uuidField.set(resume, "yet_another_new_uuid");
+        Method toStringMethod = resumeClass.getDeclaredMethod("toString");
+        String resumeUuid = (String) toStringMethod.invoke(resume);
+        System.out.println("Yet another new field value: " + resumeUuid);
+    }
+}
