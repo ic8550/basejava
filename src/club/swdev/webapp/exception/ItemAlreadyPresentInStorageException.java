fa97821d3c47e25b74a8c465af8cae01006226be
Igor Chernyavsky <ic8550@gmail.com>
2022-06-24 23:27:52 +0300

    Amend message in ItemAlreadyPresentInStorageException
    Minor textual amendment.

--- src/club/swdev/webapp/exception/ItemAlreadyPresentInStorageException.java
+++ src/club/swdev/webapp/exception/ItemAlreadyPresentInStorageException.java
@@ -2,6 +2,6 @@ package club.swdev.webapp.exception;
 
 public class ItemAlreadyPresentInStorageException extends StorageException {
     public ItemAlreadyPresentInStorageException(String uuid) {
-        super("Resume " + uuid + " already present in storage", uuid);
+        super("Resume with uuid=\\\"\" + uuid + \"\\\" already present in storage", uuid);
     }
 }
