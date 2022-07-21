d7f436103c1742acb0a275262f390432bccd59dc
Igor Chernyavsky <ic8550@gmail.com>
2022-07-21 13:35:20 +0300

    Refactor MainConcurrency to remove duplicated code

--- src/club/swdev/webapp/MainConcurrency.java
+++ src/club/swdev/webapp/MainConcurrency.java
@@ -5,39 +5,36 @@ public class MainConcurrency {
     public static final Object lock2 = new Object();
 
     public static void main(String[] args) throws InterruptedException {
+        Thread thread1 = createThread(lock1, lock2,
+                10,
+                "Thread1:    Holding lock1, waiting for lock2...",
+                "Thread1:    Holding lock1 and lock2, executing normally...");
+        Thread thread2 = createThread(lock2, lock1,
+                10,
+                "Thread2:    Holding lock2, waiting for lock1...",
+                "Thread2:    Holding lock2 and lock1, executing normally...");
         thread1.start();
         thread2.start();
-        thread1.join();
-        thread2.join();
     }
 
-    private static final Thread thread1 = new Thread(() -> {
-        synchronized (lock1) {
-            System.out.println("Thread1:    Holding lock1, waiting for lock2...");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
-            synchronized (lock2) {
-                System.out.println("Thread1:    Holding lock1 and lock2, executing normally...");
-            }
-        }
-    });
-
-    private static final Thread thread2 = new Thread(() -> {
-        synchronized (lock2) {
-            System.out.println("Thread2:    Holding lock2, waiting for lock1...");
-            try {
-                Thread.sleep(10);
-            } catch (InterruptedException e) {
-                System.out.println(e.getMessage());
-                e.printStackTrace();
-            }
+    public static Thread createThread(Object lock1,
+                                      Object lock2,
+                                      int pause,
+                                      String messageWaiting,
+                                      String messageExecuting) {
+        return new Thread(() -> {
             synchronized (lock1) {
-                System.out.println("Thread2:    Holding lock2 and lock1, executing normally...");
+                System.out.println(messageWaiting);
+                try {
+                    Thread.sleep(pause);
+                } catch (InterruptedException e) {
+                    System.out.println(e.getMessage());
+                    e.printStackTrace();
+                }
+                synchronized (lock2) {
+                    System.out.println(messageExecuting);
+                }
             }
-        }
-    });
+        });
+    }
 }
