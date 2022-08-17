fc1478f75ded84380d2d2a2895c14ee916eaa9d2
Igor Chernyavsky <ic8550@gmail.com>
2022-08-18 01:38:26 +0300

    Revert "Rename storage exception classes"
    This reverts commit ae4edfe7d8e3e3591874aa84d31bfbb2eb0862ad.

--- src/club/swdev/webapp/exception/ItemAlreadyPresentException.java
+++
@@ -1,7 +0,0 @@
-package club.swdev.webapp.exception;
-
-public class ItemAlreadyPresentException extends StorageException {
-    public ItemAlreadyPresentException(String uuid) {
-        super("Resume with uuid=\\\"\" + uuid + \"\\\" already present in storage", uuid);
-    }
-}
--- src/club/swdev/webapp/exception/ItemNotPresentException.java
+++
@@ -1,7 +0,0 @@
-package club.swdev.webapp.exception;
-
-public class ItemNotPresentException extends StorageException {
-    public ItemNotPresentException(String uuid) {
-        super("Resume with uuid=\"" + uuid + "\" not present in storage", uuid);
-    }
-}
