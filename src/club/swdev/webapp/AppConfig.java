defd76bae0206d4ea6e3567c85687197125e9b40
Igor Chernyavsky <ic8550@gmail.com>
2022-09-30 22:50:21 +0300

    Refactor AppConfig class for deployment to Heroku
    Refactor the way how we read the contents of app.properties so that
    we no longer need to access localhost's hard drive for that.

--- src/club/swdev/webapp/AppConfig.java
+++ src/club/swdev/webapp/AppConfig.java
@@ -4,15 +4,13 @@ import club.swdev.webapp.storage.SqlStorage;
 import club.swdev.webapp.storage.Storage;
 
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.util.Properties;
 
 public class AppConfig {
-    // private static final File APP_PROPS_FILE = new File("C:\\Users\\Igor\\Documents\\Topics\\Programming\\Projects\\grigory-kislin\\basejava\\config\\app.properties");
-    private static final File APP_PROPS_FILE = new File(AppConfig.getAppConfigBaseDir(), "config/app.properties");
-    // private static final String APP_PROPS_FILE = "/app.properties";
+    private static final String APP_PROPS_FILE = "/app.properties";
+
     private static final AppConfig APP_CONFIG_INSTANCE = new AppConfig();
 
     private final File storageDir;
@@ -23,10 +21,9 @@ public class AppConfig {
     }
 
     private AppConfig() {
-        try (InputStream is = new FileInputStream(APP_PROPS_FILE)) {
-        // try (InputStream inputStream = AppConfig.class.getResourceAsStream(APP_PROPS_FILE)) {
+        try (InputStream inputStream = AppConfig.class.getResourceAsStream(APP_PROPS_FILE)) {
             Properties appProps = new Properties();
-            appProps.load(is);
+            appProps.load(inputStream);
             storageDir = new File(appProps.getProperty("storage.dir"));
             storage = new SqlStorage(appProps.getProperty("db.url"), appProps.getProperty("db.user"), appProps.getProperty("db.password"));
         } catch (IOException e) {
@@ -40,14 +37,5 @@ public class AppConfig {
 
     public Storage getStorage() {
         return storage;
-    }
-
-    private static File getAppConfigBaseDir() {
-        String appProp = System.getProperty("appConfigFileBaseDir");
-        File baseDir = new File(appProp == null ? "." : appProp);
-        if (!baseDir.isDirectory()) {
-            throw new IllegalStateException(baseDir + " is not directory");
-        }
-        return baseDir;
     }
 }
